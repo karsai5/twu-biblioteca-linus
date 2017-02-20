568e09996d9501481c7d7071cfaebd0cce9930d1
Linus Karsai <karsai5@gmail.com>
2017-02-20 20:52:40 +1100

    Completed first two user stories: Welcom message and List books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,45 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
 
+    private ArrayList<String> books = new ArrayList<String>();
+
+    public BibliotecaApp() {
+        this.start();
+    }
+
+    private void initialiseBookList() {
+        books.add("Hitchhiker's Guide to the Galaxy");
+        books.add("The Princess Bride");
+        books.add("The Sparrow");
+        books.add("Ender's Game");
+        books.add("The Moon is a Harsh Mistress");
+        books.add("The Name of the Wind");
+    }
+
+    public void printWelcome() {
+        System.out.println("Welcome to Biblioteca, the home of books.");
+        System.out.println("Made my Linus Karsai for TWU.");
+        System.out.println("--");
+    }
+
+    public void start() {
+        initialiseBookList();
+        printWelcome();
+        printBooks();
+    }
+
+    public void printBooks() {
+        for (String book : books){
+           System.out.println(book);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp ba = new BibliotecaApp();
+        ba.start();
+        ba.printBooks();
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,61 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    // used for checking output of System.out.println
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        // reset streams for checking output
+        resetStreams();
+    }
+
+    @After
+    public void cleanUpStreams() {
+        // remove streams for checking output
+        System.setOut(null);
+        System.setErr(null);
+    }
+
+    public void resetStreams() {
+        // create streams for checking output
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+        outContent.reset();
+        errContent.reset();
+    }
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void checkWelcomeMessage() {
+        new BibliotecaApp();
+        assertTrue(outContent.toString().contains("Welcome"));
+        assertTrue(outContent.toString().contains("Biblioteca"));
+    }
+
+    @Test
+    public void checkBooksPrintCorrectly() {
+        BibliotecaApp ba = new BibliotecaApp();
+        assertTrue(outContent.toString().contains("The Princess Bride"));
+        assertTrue(outContent.toString().contains("Hitchhiker's Guide to the Galaxy"));
+        assertTrue(outContent.toString().contains("The Princess Bride"));
+        assertTrue(outContent.toString().contains("The Sparrow"));
+        assertTrue(outContent.toString().contains("Ender's Game"));
+        assertTrue(outContent.toString().contains("The Moon is a Harsh Mistress"));
+        assertTrue(outContent.toString().contains("The Name of the Wind"));
     }
 }
