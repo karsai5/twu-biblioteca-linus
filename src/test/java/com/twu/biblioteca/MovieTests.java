69f21fcfbdb3f9d62d7cf940d601d69fca8c9349
Linus Karsai <lkarsai@thoughtworks.com>
2017-03-06 09:29:49 +1100

    Clean up movie tests.

--- src/test/java/com/twu/biblioteca/MovieTests.java
+++ src/test/java/com/twu/biblioteca/MovieTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by linus on 23/2/17.
@@ -15,50 +16,42 @@ public class MovieTests extends BaseTest {
 
     @Override
     protected void initialiseDummyData() {
-        STATION_AGENT = new Movie("The Station Agent", "2003", "Tom McCarthy");
-        BRAVE = new Movie("Brave", "2012", "Mark Andrews");
-        biblioteca.addRentable(STATION_AGENT);
-        biblioteca.addRentable(BRAVE);
-    }
-
-    public void checkForMovieTitleText() {
-        checkForString(STATION_AGENT.getTitle());
-        checkForString(BRAVE.getTitle());
     }
 
     @Test
-    public void check_movies_titles_are_listed_correctly() {
+    public void check_title_is_printed() {
+        biblioteca.addRentable(new Movie("The Station Agent", "2003", "Tom McCarthy"));
         biblioteca.printRentables();
-        checkForMovieTitleText();
+        checkForString("The Station Agent");
     }
 
     @Test
-    public void movies_print_director_year_and_rating() {
+    public void check_year_and_director_are_printed() {
+        biblioteca.addRentable(new Movie("The Station Agent", "2003", "Tom McCarthy"));
         biblioteca.printRentables();
-        checkForString(STATION_AGENT.getDirector());
-        checkForString(STATION_AGENT.getYear());
-        checkForString(Integer.toString(STATION_AGENT.getRating()));
-        checkForString(BRAVE.getDirector());
-        checkForString(BRAVE.getYear());
-        checkForString(Integer.toString(BRAVE.getRating()));
+        checkForString("Tom McCarthy");
+        checkForString("2003");
     }
 
     @Test
-    public void checkout_the_station_agent() {
-        assertFalse(STATION_AGENT.isCheckedOut());
+    public void checkout_movie_via_biblioteca() {
+        Movie stationAgent = new Movie("The Station Agent", "2003", "Tom McCarthy");
+        biblioteca.addRentable(stationAgent);
+
         biblioteca.login(JEAN_USERNAME, JEAN_PASS);
-        biblioteca.checkout(STATION_AGENT.getTitle());
+        biblioteca.checkout("The Station Agent");
 
-        biblioteca.printRentables();
-        checkForMissingString(STATION_AGENT.getTitle());
+        assertTrue(stationAgent.isCheckedOut());
     }
 
     @Test
-    public void return_the_station_agent() {
-        STATION_AGENT.checkout(JEAN);
-        assertTrue("Station agent isn't checked out.", STATION_AGENT.isCheckedOut());
-        STATION_AGENT.checkout(JEAN);
-        biblioteca.checkin(STATION_AGENT.getTitle());
-        assertFalse("Station agent didn't check in.", STATION_AGENT.isCheckedOut());
+    public void return_movie_via_biblioteca() {
+        Movie stationAgent = new Movie("The Station Agent", "2003", "Tom McCarthy");
+        biblioteca.addRentable(stationAgent);
+
+        stationAgent.checkout(mock(User.class));
+
+        biblioteca.checkin("The Station Agent");
+        assertFalse("Station agent didn't check in.", stationAgent.isCheckedOut());
     }
 }
